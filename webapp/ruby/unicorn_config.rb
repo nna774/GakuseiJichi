d0d0f7cdf9c6a4a2ee63dcd88420eae7eaa59f01
Kazato Sugimoto <uiureo@gmail.com>
2015-09-26 13:36:52 +0900

    Update unicorn_config.rb

--- webapp/ruby/unicorn_config.rb
+++ webapp/ruby/unicorn_config.rb
@@ -1,4 +1,6 @@
-worker_processes 1
+worker_processes 4
 preload_app true
-listen 8080
 pid "/home/isucon/webapp/ruby/unicorn.pid"
+listen "/tmp/unicorn.sock", backlog: 1024
+stderr_path '/home/isucon/webapp/ruby/log/unicorn_err.log'
+stdout_path '/home/isucon/webapp/ruby/log/unicorn.log'
